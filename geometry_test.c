fcaa19a233cb9e042565e4e48a922576bdedfa87
Selena Quintanilla <sequ5848@colorado.edu>
2018-04-16 16:38:23 -0600

    last message

--- geometry_test.c
+++ geometry_test.c
@@ -15,8 +15,7 @@
 
 #include "geometry.h"
 
-/* coord_2d_eq Test */
-START_TEST(test_2d_eq)
+START_TEST(test_2d_area_triangle)
 {
     coord_2d_t a;
     coord_2d_t b;
@@ -39,6 +38,39 @@ START_TEST(test_2d_eq)
     ck_assert(!coord_2d_area_triangle(&a, &b, &c));
 }
 END_TEST
+
+/* coord_2d_eq Test */
+START_TEST(test_2d_eq)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    a.x = b.x = 0;
+    a.y = b.y = 0;
+    ck_assert(coord_2d_eq(&a, &b));
+    a.x = b.x = 9.99;
+    a.y = b.y = 9.99;
+    ck_assert(coord_2d_eq(&a, &b));
+    a.x = b.x = 3.33;
+    a.y = b.y = 9.99;
+    ck_assert(coord_2d_eq(&a, &b));
+    a.x = 3.33;
+    a.y = 9.99;
+    b.x = 3.33;
+    b.y = 10.99;
+    ck_assert(!coord_2d_eq(&a, &b));
+    a.x = 3.33;
+    a.y = 9.99;
+    b.x = 2.33;
+    b.y = 9.99;
+    ck_assert(!coord_2d_eq(&a, &b));
+    a.x = 1.11;
+    a.y = 2.22;
+    b.x = 7.77;
+    b.y = 8.88;
+    ck_assert(!coord_2d_eq(&a, &b));
+}
+END_TEST
+
 
 /* coord_2d_dist Test */
 START_TEST(test_2d_dist)
@@ -148,10 +180,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_midpoint = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle;
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
