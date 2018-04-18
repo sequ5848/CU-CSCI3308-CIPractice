5e6e50b48df3f48b0a2554ad7c7f6e55bee05688
sequ5848 <sequ5848@colorado.edu>
2018-04-17 18:04:23 -0600

    L

--- geometry_test.c
+++ geometry_test.c
@@ -15,30 +15,6 @@
 
 #include "geometry.h"
 
-START_TEST(test_2d_area_triangle)
-{
-    coord_2d_t a;
-    coord_2d_t b;
-    coord_2d_t c;
-
-    a.x = b.x = c.x = 0;
-    a.y = b.y = c.y = 0;
-    ck_assert(coord_2d_area_triangle(&a, &b, &c));
-
-    a.x = b.x = c.x = 1;
-    a.y = b.y = c.y = 1;
-    ck_assert(coord_2d_area_triangle(&a, &b, &c));
-
-    a.x = 3.33;
-    a.y = 9.99;
-    b.x = 3.33;
-    b.y = 10.99;
-    c.x = 2.33;
-    c.y = 12.33;
-    ck_assert(!coord_2d_area_triangle(&a, &b, &c));
-}
-END_TEST
-
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -163,6 +139,23 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    double area = coord_2d_area_triangle(&a, &b, &c);
+    ck_assert(area == 0);
+
+    a.x = b.x = c.x = 1;
+    a.y = b.y = c.y = 1;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -180,8 +173,8 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
-    TCase* tc_2d_midpoint = tcase_create("coord_2d_area_triangle");
-    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle;
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
